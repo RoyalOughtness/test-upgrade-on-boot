75e91ec20aef4b958fdd6db215e11eb5d64cf0c0
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2026-04-05 19:12:53 -0700

    testing

--- files/scripts/setplymouththemetext.sh
+++ files/scripts/setplymouththemetext.sh
@@ -9,4 +9,4 @@ sed -i 's/Installing Updates/Downloading Updates/' /usr/share/plymouth/themes/bg
 
 systemctl enable upgrade-on-boot.service
 
-dnf install -y java-latest-openjdk libreoffice firefox
+dnf install -y java-latest-openjdk chromium
